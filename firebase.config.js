4b75c7fbac08ca08af128fed67673fd44b621737
Nur Ertem Unden <Nur Ertem Unden>
2016-11-21 23:26:13 +0300

    Signed-off-by: Nur Ertem Unden <Nur Ertem Unden>

--- firebase.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	apiKey: "AIzaSyCqrxEYHeBjKhDiHXz4cqXidQnFQKWFBoc",
-    authDomain: "karmacircle-fea24.firebaseapp.com",
-    databaseURL: "https://karmacircle-fea24.firebaseio.com",
-    storageBucket: "karmacircle-fea24.appspot.com",
-    messagingSenderId: "263238451332"
-}
