3027a579fe06ed6373ff127f8362cc04f2c301b8
NurErtem <ertemnur@gmail.com>
2016-12-11 01:22:32 +0300

    server rendering

--- server/engine/engine/activites.js
+++ server/engine/engine/activites.js
@@ -1,10 +1,138 @@
 var activities = [
 	{
-		title: 'running',
-		calories: 45
+		title: 'Aerobics',
+		calories: 2.54
 	},
 	{
-		title: 'walking',
-		calories: 83
+		title: 'Step Aerobics',
+		calories: 3.20
 	},
+    {
+        title: 'Badminton',
+        calories: 3.31
+    },
+    {
+        title: 'Basketball',
+        calories: 4.85
+    },
+    {
+        title: 'Bicycling',
+        calories: 2.76
+    },
+    {
+        title: 'Bowling',
+        calories: 1.21
+    },
+    {
+        title: 'Canoeing',
+        calories: 1.54
+    },
+    {
+        title: 'Cross Country Ski',
+        calories: 4.85
+    },
+    {
+        title: 'Dancing',
+        calories: 2.20
+    },
+    {
+        title: 'Golfing',
+        calories: 2.20
+    },
+    {
+        title: 'Handball',
+        calories: 5.07
+    },
+    {
+        title: 'Hiking',
+        calories: 4.41
+    },
+    {
+        title: 'Jogging 8kph',
+        calories: 4.08
+    },
+    {
+        title: 'Jogging 10kph',
+        calories: 5.07
+    },
+    {
+        title: 'Raquetball',
+        calories: 4.52
+    },
+    {
+        title: 'Rowing Machine',
+        calories: 3.97
+    },
+    {
+        title: 'Running 13kph',
+        calories: 6.72
+    },
+    {
+        title: 'Running 16kph',
+        calories: 7.72
+    },
+    {
+        title: 'Skipping Rope',
+        calories: 6.28
+    },
+    {
+        title: 'Skiing downhill',
+        calories: 2.87
+    },
+    {
+        title: 'Soccer',
+        calories: 4.30
+    },
+    {
+        title: 'Squash',
+        calories: 4.52
+    },
+    {
+        title: 'Stair Climbing',
+        calories: 3.09
+    },
+    {
+        title: 'Swimming 25 mpm',
+        calories: 2.65
+    },
+    {
+        title: 'Swimming 50mpm',
+        calories: 4.96
+    },
+    {
+        title: 'Tennis',
+        calories: 2.43
+    },
+    {
+        title: 'Volleyball',
+        calories: 1.54
+    },
+    {
+        title: 'Walking 3kph',
+        calories: 1.32
+    },
+    {
+        title: 'Walking 5kph',
+        calories: 1.76
+    },
+    {
+        title: 'Walking 6kph',
+        calories: 2.20
+    },
+    {
+        title: 'Waterski',
+        calories: 3.53
+    },
+    {
+        title: 'Walking 3kph',
+        calories: 1.32
+    },
+    {
+        title: 'Weight training fast',
+        calories: 5.62
+    },
+    {
+        title: 'Weight training moderate',
+        calories: 4.19
+    },
 ];
